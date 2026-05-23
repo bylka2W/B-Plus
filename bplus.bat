2eb77ea5a07dad46c3f20d8d7a33d635d029ca18
bylka2 <shvedovdim@gmail.com>
2026-05-23 12:21:31 +0300

    auto-detect VS instance in bplus.bat

--- bplus.bat
+++ bplus.bat
@@ -1,6 +1,5 @@
 @echo off
 title B+ v4.0.0
-
 echo B+ v4.0.0
 
 dotnet build src/BPlusTranspiler >nul 2>&1
@@ -11,15 +10,28 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 
-set "EXT_DIR=%LOCALAPPDATA%\Microsoft\VisualStudio\17.0_9e4753c6\Extensions\CapGames221\BPlusLanguage\4.0.0"
+rem Find the latest Visual Studio instance
+for /f "delims=" %%i in ('dir "%LOCALAPPDATA%\Microsoft\VisualStudio\*.*_" /b /o:-n 2^>nul') do (
+    set "VS_INSTANCE=%%i"
+    goto :found
+)
+:found
+
+echo Installing to Visual Studio: %VS_INSTANCE%
+
+set "EXT_DIR=%LOCALAPPDATA%\Microsoft\VisualStudio\%VS_INSTANCE%\Extensions\CapGames221\BPlusLanguage\4.0.0"
 if not exist "%EXT_DIR%" mkdir "%EXT_DIR%"
 copy /y src\vs-extension\BPlusLanguage\bin\Release\BPlusLanguage.dll "%EXT_DIR%\BPlusLanguage.dll" >nul
 copy /y src\vs-extension\BPlusLanguage\source.extension.vsixmanifest "%EXT_DIR%\extension.vsixmanifest" >nul
 
-rem Clear extension cache so VS picks up new extension
-del /f /q "%LOCALAPPDATA%\Microsoft\VisualStudio\17.0_9e4753c6\Extensions\ExtensionMetadata.mpack" >nul 2>&1
-del /f /q "%LOCALAPPDATA%\Microsoft\VisualStudio\17.0_9e4753c6\Extensions\ExtensionMetadataCache.sqlite" >nul 2>&1
+rem Write Content_Types if not exists
+if not exist "%EXT_DIR%\[Content_Types].xml" copy /y src\vs-extension\BPlusLanguage\bin\Release\BPlusLanguage.dll "%EXT_DIR%\[Content_Types].xml" >nul 2>&1
 
-echo Extension installed. Starting Visual Studio...
+rem Clear extension cache
+del /f /q "%LOCALAPPDATA%\Microsoft\VisualStudio\%VS_INSTANCE%\Extensions\ExtensionMetadata.mpack" >nul 2>&1
+del /f /q "%LOCALAPPDATA%\Microsoft\VisualStudio\%VS_INSTANCE%\Extensions\ExtensionMetadataCache.sqlite" >nul 2>&1
+del /f /q "%LOCALAPPDATA%\Microsoft\VisualStudio\%VS_INSTANCE%\Extensions\extensions.configurationchanged" >nul 2>&1
+
+echo Done. Starting Visual Studio...
 start devenv
 exit
